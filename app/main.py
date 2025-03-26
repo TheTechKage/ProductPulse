5a9f5198daa73ef6d70bd6722f063fde0c9096a8
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-03-27 00:06:03 +0530

    added db connection

--- app/main.py
+++ app/main.py
@@ -1,19 +1,41 @@
 from fastapi import FastAPI
+from database.db import DatabasePool
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
+if __name__ == "__main__":
+    try:
+        load_dotenv()
+        connection_pool=DatabasePool(
+            os.getenv("DB_POOL_MIN_CONNECTION"),
+            os.getenv("DB_POOL_MAX_CONNECTION"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_DB_PORT"),
+            database=os.getenv("POSTGRES_DB")
+        )
 
-@app.get("/api/products")
-async def get_products():
-    return {}
+        connection =connection_pool.get_connection()
 
-@app.get("/api/product/{id}")
-async def get_product(id: int):
-    return {}
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-@app.get("/api/categories")
-async def get_categories():
-    return {}
 
-@app.get("/api/cart")
-async def get_cart():
-    return {}
+# app = FastAPI()
+
+# @app.get("/api/products")
+# async def get_products():
+#     return {}
+
+# @app.get("/api/product/{id}")
+# async def get_product(id: int):
+#     return {}
+
+# @app.get("/api/categories")
+# async def get_categories():
+#     return {}
+
+# @app.get("/api/cart")
+# async def get_cart():
+#     return {}
