da31d51b2430738fc47d19f2faac172ee7c9ffae
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2025-03-28 22:49:45 +0530

    refactor: moved the logic to load data

--- app/main.py
+++ app/main.py
@@ -1,24 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas.product import Product
 from app.schemas.category import Category
+from app.utils import load_data
 from typing import List, Dict
-import json
 
 app = FastAPI()
 
 
 @app.get("/api/products", response_model=List[Product])
 async def get_products():
-    with open("./data.json", "r") as f:
-        data = json.load(f)
+    data = load_data()
     return data.get("products")
 
 
 @app.get("/api/product/{id}", response_model=Product)
 async def get_product(id: int):
-    with open("./data.json", "r") as f:
-        data = json.load(f)
-
+    data = load_data()
     products = data.get("products", [])
     for product in products:
         if product["id"] == id:
@@ -29,8 +26,7 @@ async def get_product(id: int):
 
 @app.get("/api/categories", response_model=Dict[str, Category])
 async def get_categories():
-    with open("./data.json", "r") as f:
-        data = json.load(f)
+    data = load_data()
     return data.get("categories", {})
 
 
---
+++ app/utils.py
@@ -0,0 +1,12 @@
+from fastapi import HTTPException
+import json
+
+
+def load_data():
+    try:
+        with open("app/data.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found")
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Invalid data format")
