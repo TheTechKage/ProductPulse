9c8bba814766f4a0fc19abbe79f55568af9d64c0
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-03-30 11:15:56 +0530

    uncommeented existing dummy apis

--- app/main.py
+++ app/main.py
@@ -42,20 +42,20 @@ if __name__ == "__main__":
         print(f"An error occurred in application: {e}")
 
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/api/products")
-# async def get_products():
-#     return {}
+@app.get("/api/products")
+async def get_products():
+    return {}
 
-# @app.get("/api/product/{id}")
-# async def get_product(id: int):
-#     return {}
+@app.get("/api/product/{id}")
+async def get_product(id: int):
+    return {}
 
-# @app.get("/api/categories")
-# async def get_categories():
-#     return {}
+@app.get("/api/categories")
+async def get_categories():
+    return {}
 
-# @app.get("/api/cart")
-# async def get_cart():
-#     return {}
+@app.get("/api/cart")
+async def get_cart():
+    return {}
